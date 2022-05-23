9a3c38b5a113c0cf6e901d6da5d20b31ba72803d
capuanomaurizio <mauricapuano@gmail.com>
2022-05-23 12:10:17 +0200

    Caricamento progetto

---
+++ app/src/main/java/com/example/api_json_request/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.api_json_request;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button requestButton;
+    private TextView requestResult;
+    RequestQueue queue;
+    StringRequest stringRequest;
+    String url;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        requestButton = (Button)findViewById(R.id.requestButton);
+        requestResult = (TextView)findViewById(R.id.requestResult);
+
+        requestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getData();
+            }
+        });
+    }
+
+    private void getData() {
+        url = "https://run.mocky.io/v3/15dc16fb-f6f8-4315-b5df-a8d442766c3d";
+        queue = Volley.newRequestQueue(this);
+        stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        requestResult.setText(response);
+                        Log.d("PROBLEMISSIMOOOOOO", response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        requestResult.setText("Errore nella richiesta");
+                        Log.d("PROBLEMISSIMOOOOOO", "ERROR");
+                    }
+                });
+        queue.add(stringRequest);
+    }
+}
